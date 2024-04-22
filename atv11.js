6d337a88a973779c36eba02106f38d296cddb908
Lucasvvieiraa <lucas_vieira9@estudante.sesisenai.org.br>
2024-04-22 09:43:07 -0300

    calculadora.pronta

--- atv11.js
+++ atv11.js
@@ -3,30 +3,30 @@ function Calculadora () {
     const operação = document.documentElementById("operação").value;
     const n1 = parseflloat = parsefloat(document.getElementById("n1").value);
     const n2 = paresefloat(document.getElementById("n2").value);
+    var result = document.querySelector('span');
     let results;
 
     switch (operation){
         case 'adicao':
-            result = n1 + n2;
+            result.innerhtml = n1 + n2;
             break;
         case 'subtração':
-                result = n1 - n2;
+                result.innerhtml = n1 - n2;
                 break;
         case'multiplicacao':
-                result = n1 * n2;
+                result.innerhtml = n1 * n2;
                 break;
         case 'divisao':
             if(n2 !== 0) {
-                result = n1 / n2;
+                result.innerhtml = n1 / n2;
             } else{
-                result = 'Inválido, divisão por zero';
+                result.innerhtml = 'Inválido, divisão por zero';
             }
             break;
             default:
-                result = 'Operação inválida';
+                result.innerhtml = 'Operação inválida';
                 break;
         
     }
-    document.getElementById("result").value = result;
     
 }
